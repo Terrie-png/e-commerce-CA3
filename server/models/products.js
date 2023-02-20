6301b14d66ac2a08b90554f13a19d97b72438462
Raveena Rathnam <D00251856@student.dkit.ie>
2023-02-20 23:05:22 +0000

    Update products.js

--- server/models/products.js
+++ server/models/products.js
@@ -3,14 +3,14 @@ const mongoose = require(`mongoose`)
 let productsSchema = new mongoose.Schema(
     {
         name:{type:String ,required:true},
-        brand: {type:String},
-        gender: {type:String},
-        category: {type: String},
-        price: {type: Number},
-        is_in_inventory: {type:Boolean},
-        items_left: {type:Number},
+        brand: {type:String,required:true,enum:["NIKE", "HUSHPUPPIES","ADIDAS","Reebok","Vans"]},
+        gender: {type:String,required:true,enum:["MEN","WOMEN","KIDS"]},
+        category: {type: String,required:true,enum:["RUNNING", "FOOTBALL","CASUAL","FORMAL"]},
+        price: {type: Number,required:true,min:0, max:500},
+        is_in_inventory: {type:Boolean,default: false },
+        items_left: {type:Number,required:true,min:0},
         imageURL: {type:String ,default:""},
-        slug: {type: String}
+        slug: {type: String,required:true}
     },
     {
         collection: `products`
